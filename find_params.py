12251514cbe5cb003da2bd26777b688a349fa4c6
soloiaros <soloiaros@gmail.com>
2024-02-09 00:11:56 +0300

    zoom in/zoom out added

--- find_params.py
+++ find_params.py
@@ -1,10 +1,8 @@
-def find_params(coords):
-
+def find_params(coords, spn):
     toponym_longitude, toponym_lattitude = coords
-
     map_params = {
         "ll": ",".join([toponym_longitude, toponym_lattitude]),
-        "spn": ",".join(['0.005', '0.005']),
+        "spn": ",".join([str(spn), str(spn)]),
         "l": "map",
     }
 
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 from PyQt5 import uic
+from PyQt5.QtCore import Qt
 from io import BytesIO
 import requests
 from PyQt5.QtGui import QPixmap
@@ -11,13 +12,23 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('ui/first_ui.ui', self)
+        self.setWindowTitle('YandexMaps')
         self.pushButton.clicked.connect(self.show_map)
+        self.spn = 0.002
 
     def show_map(self):
         coords = (self.lineEdit.text(), self.lineEdit_2.text())
-        get_image(coords)
+        get_image(coords, self.spn)
         pixmap = QPixmap('data/map.png')
         self.label.setPixmap(pixmap)
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_PageUp:
+            self.spn = max([0.002, self.spn - 0.005])
+            self.show_map()
+        if event.key() == Qt.Key_PageDown:
+            self.spn = min([0.050, self.spn + 0.005])
+            self.show_map()
 
 
 def exception_hook(cls, exception, traceback):
